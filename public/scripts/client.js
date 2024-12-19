2491c8f7b63b47de70098ef77af51765e3551c6f
Javin Leslie Lau <javinlau21@gmail.com>
2024-12-19 11:17:06 -0500

    19 1116 Dec 24 fetching tweets with ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,30 +5,30 @@
  */
 
 $(document).ready(function () {
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
+  // const data = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png",
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@rd"
+  //     },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1461113959088
+  //   }
+  // ];
 
   const createTweetElement = function (tweet) {
     const $tweet = $(`
@@ -67,7 +67,7 @@ $(document).ready(function () {
   };
 
   // Render the tweets
-  renderTweets(data);
+  // renderTweets(data);
 
   // Add event listener for form submission
   $('.new-tweet form').on('submit', function (event) {
@@ -92,4 +92,20 @@ $(document).ready(function () {
       }
     });
   });
+
+  const loadTweets = function () {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      dataType: 'json',
+      success: function (tweets) {
+        renderTweets(tweets);
+      },
+      error: function (error) {
+        console.error('Error fetching tweets:', error);
+      }
+    });
+  };
+
+  loadTweets();
 });
